6aa38b945632a67af5a59e897a991c044acbadfc
Lucas Bottrel <lucas.bottrel@levty.com>
2022-02-16 22:37:34 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule
