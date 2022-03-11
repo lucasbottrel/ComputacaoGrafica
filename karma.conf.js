735222bfbff46df12e0a4262d781308dc1aabe58
Lucas Bottrel <lucas.bottrel@levty.com>
2022-03-11 14:34:17 -0300

    Change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/computacao-paralela-unidade-um'),
+      dir: require('path').join(__dirname, './coverage/computacao-grafica-unidade-um'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'computacao-paralela-unidade-um';
+  title = 'computacao-grafica-unidade-um';
 }
